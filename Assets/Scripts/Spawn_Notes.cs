5b6f47881ff7d95210b1b989fd381829d1c23675
origamisith <lukeboarder@gmail.com>
2021-10-30 21:53:22 -0700

    boom for real this time

--- Assets/Scripts/Spawn_Notes.cs
+++ Assets/Scripts/Spawn_Notes.cs
@@ -7,6 +7,7 @@ public class Spawn_Notes : MonoBehaviour
     public int bpm;
     public float songDelay;
     public GameObject beat;
+    public GameObject explosion;
     public AudioClip song;
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,7 @@ public class Spawn_Notes : MonoBehaviour
 
     private void SpawnNote()
     {
-        Instantiate(beat, transform.GetChild(Random.Range(0,6)));
+        GameObject cannon = Instantiate(beat, transform.GetChild(Random.Range(0, 6))).gameObject;
+        Instantiate(explosion, cannon.transform).transform.parent= gameObject.transform;
     }
 }
